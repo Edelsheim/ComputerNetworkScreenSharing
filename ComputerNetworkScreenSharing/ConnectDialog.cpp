22a38a8b1efe6c26208837e1224242d6a8f2de4e
Edelsheim <skyline7651@gmail.com>
2020-06-28 01:38:46 +0900

    add to ConnectDialog

---
+++ ComputerNetworkScreenSharing/ConnectDialog.cpp
@@ -0,0 +1,77 @@
+﻿// ConnectDialog.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "ComputerNetworkScreenSharing.h"
+#include "ConnectDialog.h"
+#include "afxdialogex.h"
+
+
+// ConnectDialog 대화 상자
+
+IMPLEMENT_DYNAMIC(ConnectDialog, CDialogEx)
+
+ConnectDialog::ConnectDialog(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_ConnectDialog, pParent)
+{
+
+}
+
+ConnectDialog::~ConnectDialog()
+{
+}
+
+void ConnectDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_ServerIP, ConnectIP);
+	DDX_Control(pDX, IDC_ServerPort, ConnectPort);
+}
+
+
+BEGIN_MESSAGE_MAP(ConnectDialog, CDialogEx)
+	ON_BN_CLICKED(IDOK, &ConnectDialog::OnBnClickedOk)
+END_MESSAGE_MAP()
+
+
+// ConnectDialog 메시지 처리기
+
+
+BOOL ConnectDialog::DestroyWindow()
+{
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+
+	return CDialogEx::DestroyWindow();
+}
+
+
+BOOL ConnectDialog::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	BOOL check;
+
+	CString defaultConnectIPStr;
+	check = defaultConnectIPStr.LoadStringW(IDS_DefaultConnectIP);
+	ASSERT(check);
+	ConnectIP.SetWindowTextW(defaultConnectIPStr);
+
+	CString defaultConnectPortStr;
+	check = defaultConnectPortStr.LoadStringW(IDS_DefaultConnectPort);
+	ASSERT(check);
+	ConnectPort.SetWindowTextW(defaultConnectPortStr);
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+				  // 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
+}
+
+
+void ConnectDialog::OnBnClickedOk()
+{
+	BYTE a, b, c, d;
+	ConnectIP.GetAddress(a, b, c, d);
+	ip.Format(_T("%d.%d.%d.%d"), a, b, c, d);
+
+	ConnectPort.GetWindowTextW(port);
+	CDialogEx::OnOK();
+}
---
+++ ComputerNetworkScreenSharing/ConnectDialog.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+
+// ConnectDialog 대화 상자
+#include <iostream>
+
+class ConnectDialog : public CDialogEx
+{
+	DECLARE_DYNAMIC(ConnectDialog)
+
+public:
+	ConnectDialog(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~ConnectDialog();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ConnectDialog };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+
+	CIPAddressCtrl ConnectIP;
+	CEdit ConnectPort;
+
+public:
+	virtual BOOL DestroyWindow();
+	virtual BOOL OnInitDialog();
+	afx_msg void OnBnClickedOk();
+
+	CString ip;
+	CString port;
+};
